25a1fb6dc66732d5316956747c99b9710d503c84
Jacofig <56625982+Jacofig@users.noreply.github.com>
2026-03-19 00:15:05 +0100

    Time: 0 ms (100.00%) | Memory: 42.8 MB (87.43%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+
+        int firstCol = 0;
+        int firstRow = 0;
+        int lastCol = matrix[0].length-1;
+        int lastRow = matrix.length-1;
+
+        while (firstCol <= lastCol && firstRow <= lastRow)
+        {
+            for (int i = firstCol; i <= lastCol; i++)
+            {
+                result.add(matrix[firstRow][i]);
+            }
+            firstRow++;
+
+            for (int i = firstRow; i <= lastRow; i++)
+            {
+                result.add(matrix[i][lastCol]);
+            }
+            lastCol--;
+
+            if (firstRow <= lastRow)
+            {
+                for (int i = lastCol; i >= firstCol; i--)
+                {
+                    result.add(matrix[lastRow][i]);
+                }
+                lastRow--;
+            }
+            
+            if (firstCol <= lastCol)
+            {
+                for (int i = lastRow; i >= firstRow; i--)
+                {
+                    result.add(matrix[i][firstCol]);
+                }
+            }
+            
+            firstCol++;
+        }
+
+        return result;
+        
+    }
+}
